6f08a8811c2da808f9bb171db854ced94937245b
ksekimoto <ksekimoto@users.noreply.github.com>
2018-09-17 12:44:33 +0900

    Changed to use rx-setjmp instead of rx specific routine

--- py/nlr.h
+++ py/nlr.h
@@ -61,7 +61,7 @@
 // ToDo
 //#elif defined (__RX__)
 //    #define MICROPY_NLR_RX (1)
-//    #define MICROPY_NLR_NUM_REGS (18)
+//    #define MICROPY_NLR_NUM_REGS (20)
 #else
     #define MICROPY_NLR_SETJMP (1)
     //#warning "No native NLR support for this arch, using setjmp implementation"
