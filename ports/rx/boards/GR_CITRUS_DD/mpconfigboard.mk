097ad18095ae78801813772152311faf525bf0a8
ksekimoto <ksekimoto@users.noreply.github.com>
2018-12-28 17:20:14 +0900

    Changed to capital for MCU_SERIES string

--- ports/rx/boards/GR_CITRUS_DD/mpconfigboard.mk
+++ ports/rx/boards/GR_CITRUS_DD/mpconfigboard.mk
@@ -1,4 +1,4 @@
-MCU_SERIES = rx63n
+MCU_SERIES = RX63N
 MCU_NUMBER = RR5F5631FDDFP
 LD_FILES = boards/GR_CITRUS_DD/gr_citrus_dd.ld
 START_FILE = rx63n/startup_rx63n.S
--- ports/rx/boards/GR_SAKURA_DD/mpconfigboard.mk
+++ ports/rx/boards/GR_SAKURA_DD/mpconfigboard.mk
@@ -1,4 +1,4 @@
-MCU_SERIES = rx63n
+MCU_SERIES = RX63N
 MCU_NUMBER = R5F563NBDDFP
 LD_FILES = boards/GR_SAKURA_DD/gr_sakura_dd.ld
 START_FILE = rx63n/startup_rx63n.S
