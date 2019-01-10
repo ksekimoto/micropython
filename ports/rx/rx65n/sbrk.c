055f63041f026eb47ff586adeae500ae20cb4c8d
ksekimoto <ksekimoto@users.noreply.github.com>
2019-01-10 22:33:12 +0900

    Updated not using malloc provided by gcc library

--- ports/rx/rx65n/sbrk.c
+++ ports/rx/rx65n/sbrk.c
@@ -26,19 +26,19 @@
 
 extern char end; /* Set by linker.  */
 extern char heapmax;
-char *heap_end = 0;
+char *_heap_end = 0;
 
 void *sbrk(int incr) {
-    char * prev_heap_end;
+    char *prev_heap_end;
 
-    if (heap_end == 0)
-        heap_end = &end;
+    if (_heap_end == 0)
+        _heap_end = &end;
 
-    if (((int)heap_end + incr) > (int)(&heapmax))
+    if (((int)_heap_end + incr) > (int)(&heapmax))
         return (void *)-1;
 
-    prev_heap_end = heap_end;
-    heap_end += incr;
+    prev_heap_end = _heap_end;
+    _heap_end += incr;
 
     return (void *)prev_heap_end;
 }
