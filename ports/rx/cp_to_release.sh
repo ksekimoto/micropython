bdd2b86b3bcb50becca7eba6479b092298c1556f
ksekimoto <ksekimoto@users.noreply.github.com>
2019-03-03 11:09:47 +0900

    Added scripts for build and release

---
+++ ports/rx/cp_to_release.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+cp build-GR_CITRUS_DD/firmware.bin ../../rx_releases/gr_citrus/latest/MPY-GR_CITRUS_DD.bin
+cp build-GR_ROSE_DD/firmware.bin ../../rx_releases/gr_rose/latest/MPY-GR_ROSE_DD.bin
+cp build-GR_SAKURA_DD/firmware.bin ../../rx_releases/gr_sakura/latest/MPY-GR_SAKURA_DD.bin
