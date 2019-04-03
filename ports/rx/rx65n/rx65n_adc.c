01d2e9ccd99ca066ae6fee0a51e872a3535f778d
ksekimoto <ksekimoto@users.noreply.github.com>
2019-04-03 20:03:32 +0900

    Added RX65N AN1xx pin support

--- ports/rx/rx65n/rx65n_adc.c
+++ ports/rx/rx65n/rx65n_adc.c
@@ -49,7 +49,7 @@ static uint8_t adc10_pin[] = {
 #define ADC10_SIZE  (sizeof(adc10_pin)/sizeof(uint8_t))
 #endif
 
-static uint8_t adc12_pin[] = {
+static uint8_t adc120_pin[] = {
     P40,    /* AN000 */
     P41,    /* AN001 */
     P42,    /* AN002 */
@@ -58,21 +58,53 @@ static uint8_t adc12_pin[] = {
     P45,    /* AN005 */
     P46,    /* AN006 */
     P47,    /* AN007 */
-    PD0,    /* AN008 */
-    PD1,    /* AN009 */
-    PD2,    /* AN010 */
-    PD3,    /* AN011 */
-    PD4,    /* AN012 */
-    PD5,    /* AN013 */
-    P90,    /* AN014 */
-    P91,    /* AN015 */
-    P92,    /* AN016 */
-    P93,    /* AN017 */
-    P00,    /* AN018 */
-    P01,    /* AN019 */
-    P02,    /* AN020 */
 };
-#define ADC12_SIZE  (sizeof(adc12_pin)/sizeof(uint8_t))
+#define ADC120_SIZE  (sizeof(adc120_pin)/sizeof(uint8_t))
+
+static uint8_t adc121_pin[] = {
+    PE2,    /* AN100 */
+    PE3,    /* AN101 */
+    PE4,    /* AN102 */
+    PE5,    /* AN103 */
+    PE6,    /* AN104 */
+    PE7,    /* AN005 */
+    PD6,    /* AN106 */
+    PD7,    /* AN107 */
+    PD0,    /* AN108 */
+    PD1,    /* AN109 */
+    PD2,    /* AN110 */
+    PD3,    /* AN111 */
+    PD4,    /* AN112 */
+    PD5,    /* AN113 */
+    P90,    /* AN114 */
+    P91,    /* AN115 */
+    P92,    /* AN116 */
+    P93,    /* AN117 */
+    P00,    /* AN118 */
+    P01,    /* AN119 */
+    P02,    /* AN120 */
+};
+#define ADC121_SIZE  (sizeof(adc121_pin)/sizeof(uint8_t))
+
+bool rx_adc_chk_ad120(uint8_t pin) {
+    int i;
+    for (i = 0; i < ADC120_SIZE; i++) {
+        if (adc120_pin[i] == pin) {
+            return true;
+        }
+    }
+    return false;
+}
+
+bool rx_adc_chk_ad121(uint8_t pin) {
+    int i;
+    for (i = 0; i < ADC121_SIZE; i++) {
+        if (adc121_pin[i] == pin) {
+            return true;
+        }
+    }
+    return false;
+}
 
 int32_t rx_adc_get_resolution(uint8_t pin) {
     int i;
@@ -84,17 +116,15 @@ int32_t rx_adc_get_resolution(uint8_t pin) {
         }
     }
 #endif
-    for (i = 0; i < ADC12_SIZE; i++) {
-        if (adc12_pin[i] == pin) {
-            return 12;
-        }
+    if (rx_adc_chk_ad120(pin) | rx_adc_chk_ad121(pin)) {
+        return 12;
     }
     return res;
 }
 
 int32_t rx_adc_get_channel(uint8_t pin) {
     int i;
-    int res = -1;
+    int ch = -1;
 #if defined(RX_ADC10)
     for (i = 0; i < ADC10_SIZE; i++) {
         if (adc10_pin[i] == pin) {
@@ -102,12 +132,17 @@ int32_t rx_adc_get_channel(uint8_t pin) {
         }
     }
 #endif
-    for (i = 0; i < ADC12_SIZE; i++) {
-        if (adc12_pin[i] == pin) {
+    for (i = 0; i < ADC120_SIZE; i++) {
+        if (adc120_pin[i] == pin) {
             return i;
         }
     }
-    return res;
+    for (i = 0; i < ADC121_SIZE; i++) {
+        if (adc121_pin[i] == pin) {
+            return i;
+        }
+    }
+    return ch;
 }
 
 #if defined(RX_ADC10)
@@ -138,7 +173,15 @@ void rx_adc12_enable(uint8_t pin) {
     uint8_t mask = GPIO_MASK(pin);
     SYSTEM.PRCR.WORD = 0xA502;
     /* 12 bit AD start */
-    SYSTEM.MSTPCRA.BIT.MSTPA17 = 0;
+    if (rx_adc_chk_ad120(pin)) {
+        if (SYSTEM.MSTPCRA.BIT.MSTPA17 == 1) {
+            SYSTEM.MSTPCRA.BIT.MSTPA17 = 0;
+        }
+    } else {
+        if (SYSTEM.MSTPCRA.BIT.MSTPA16 == 1) {
+            SYSTEM.MSTPCRA.BIT.MSTPA16 = 0;
+        }
+    }
     SYSTEM.PRCR.WORD = 0xA500;
     /* Enable write to PFSWE */
     MPC.PWPR.BIT.B0WI = 0;
@@ -149,11 +192,17 @@ void rx_adc12_enable(uint8_t pin) {
     MPC.PWPR.BYTE = 0x80;
 #if defined(RX63N)
     S12AD.ADCSR.BYTE = 0x0;
+    S12AD.ADCER.BIT.ADRFMT = 0;
 #endif
 #if defined(RX65N)
-    S12AD.ADCSR.WORD = 0x0;
+    if (rx_adc_chk_ad120(pin)) {
+        S12AD.ADCSR.WORD = 0x0;
+        S12AD.ADCER.BIT.ADRFMT = 0;
+    } else {
+        S12AD1.ADCSR.WORD = 0x0;
+        S12AD1.ADCER.BIT.ADRFMT = 0;
+    }
 #endif
-    S12AD.ADCER.BIT.ADRFMT = 0;
 }
 
 bool rx_adc_enable(uint8_t pin) {
@@ -238,15 +287,29 @@ uint16_t rx_adc12_read(uint8_t pin) {
     uint16_t off = (uint8_t)rx_adc_get_channel(pin);
 #if defined(RX63N)
     S12AD.ADANS0.WORD |= (1 << off);
-#endif
-#if defined(RX65N)
-    S12AD.ADANSA0.WORD |= (1 << off);
-#endif
     S12AD.ADCSR.BIT.ADST = 1;
     while (S12AD.ADCSR.BIT.ADST) {
         ;
     }
     value16 = *((unsigned short*)&S12AD.ADDR0 + off);
+#endif
+#if defined(RX65N)
+    if (rx_adc_chk_ad120(pin)) {
+        S12AD.ADANSA0.WORD |= (1 << off);
+        S12AD.ADCSR.BIT.ADST = 1;
+        while (S12AD.ADCSR.BIT.ADST) {
+            ;
+        }
+        value16 = *((unsigned short*)&S12AD.ADDR0 + off);
+    } else {
+        S12AD1.ADANSA0.WORD |= (1 << off);
+        S12AD1.ADCSR.BIT.ADST = 1;
+        while (S12AD1.ADCSR.BIT.ADST) {
+            ;
+        }
+        value16 = *((unsigned short*)&S12AD1.ADDR0 + off);
+    }
+#endif
     return (uint16_t)value16;
 }
 
