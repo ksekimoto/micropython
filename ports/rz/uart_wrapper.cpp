cc5db932e4613134eca99104978c05142ea738ed
ksekimoto <ksekimoto@users.noreply.github.com>
2020-03-08 12:02:54 +0900

    Fixed console input handling bug

--- ports/rz/uart_wrapper.cpp
+++ ports/rz/uart_wrapper.cpp
@@ -37,11 +37,7 @@ void mp_hal_stdout_tx_chr(int c) {
 }
 
 int mp_hal_stdin_rx_chr(void) {
-    int c = 0;
-    if (pc.readable()) {
-        c = pc.getc();
-    }
-    return c;
+    return pc.getc();
 }
 
 void mp_hal_stdout_tx_strn(const char *str, int len) {
