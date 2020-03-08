5738a93892ce3a74e7547419157682d95608be88
ksekimoto <ksekimoto@users.noreply.github.com>
2020-03-08 10:11:24 +0900

    Changed to capital file name of linker script file for GR_MANGO

--- ports/rz/boards/GR_MANGO/mpconfigboard.mk
+++ ports/rz/boards/GR_MANGO/mpconfigboard.mk
@@ -1,6 +1,6 @@
 MCU_SERIES = RZA2M
 MCU_NUMBER = RR5F565NEDDLI
-LD_FILES = boards/GR_MANGO/gr_mango.ld
+LD_FILES = boards/GR_MANGO/GR_MANGO.ld
 START_FILE = rza2m/startup_rza2m.S
 
 # MicroPython settings
