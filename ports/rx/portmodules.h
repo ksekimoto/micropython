d77a862659c69a74ac347e749c4ae2e9f3e716e8
ksekimoto <ksekimoto@users.noreply.github.com>
2020-06-21 12:38:51 +0900

    Removed stm32 module from portmodules.h

--- ports/rx/portmodules.h
+++ ports/rx/portmodules.h
@@ -27,7 +27,6 @@
 #define MICROPY_INCLUDED_RX_PORTMODULES_H
 
 extern const mp_obj_module_t pyb_module;
-extern const mp_obj_module_t stm_module;
 extern const mp_obj_module_t mp_module_uos;
 extern const mp_obj_module_t mp_module_utime;
 extern const mp_obj_module_t mp_module_usocket;
