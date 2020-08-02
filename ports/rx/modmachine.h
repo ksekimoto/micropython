c1eed51a228952e9d52223d914de49725d1b64eb
ksekimoto <ksekimoto@users.noreply.github.com>
2020-08-02 21:56:10 +0900

    Updated machine module to merge changes for stm32 port

--- ports/rx/modmachine.h
+++ ports/rx/modmachine.h
@@ -29,6 +29,7 @@
 
 #include "py/obj.h"
 
+extern const mp_obj_type_t machine_adc_type;
 extern const mp_obj_type_t machine_timer_type;
 
 void machine_init(void);
@@ -40,7 +41,14 @@ MP_DECLARE_CONST_FUN_OBJ_0(machine_unique_id_obj);
 MP_DECLARE_CONST_FUN_OBJ_0(machine_reset_obj);
 MP_DECLARE_CONST_FUN_OBJ_VAR_BETWEEN(machine_bootloader_obj);
 MP_DECLARE_CONST_FUN_OBJ_VAR_BETWEEN(machine_freq_obj);
+
+MP_DECLARE_CONST_FUN_OBJ_0(machine_idle_obj);
 MP_DECLARE_CONST_FUN_OBJ_VAR_BETWEEN(machine_lightsleep_obj);
 MP_DECLARE_CONST_FUN_OBJ_VAR_BETWEEN(machine_deepsleep_obj);
+
+MP_DECLARE_CONST_FUN_OBJ_0(machine_disable_irq_obj);
+MP_DECLARE_CONST_FUN_OBJ_VAR_BETWEEN(machine_enable_irq_obj);
+
+MP_DECLARE_CONST_FUN_OBJ_0(pyb_irq_stats_obj);
 
 #endif // MICROPY_INCLUDED_RX_MODMACHINE_H
