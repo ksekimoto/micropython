2f0f622d4c842f01cd8332f3f27dec78e9aa65b3
ksekimoto <ksekimoto@users.noreply.github.com>
2020-08-15 13:06:15 +0900

    Updated to contain ntptime.py as a pre-compiled module

--- ports/rz/boards/manifest.py
+++ ports/rz/boards/manifest.py
@@ -1,4 +1,6 @@
+freeze("$(PORT_DIR)/modules")
 include("$(MPY_DIR)/extmod/uasyncio/manifest.py")
 freeze("$(MPY_DIR)/drivers/dht", "dht.py")
 freeze("$(MPY_DIR)/drivers/display", ("lcd160cr.py", "lcd160cr_test.py"))
 freeze("$(MPY_DIR)/drivers/onewire", "onewire.py")
+freeze("$(MPY_DIR)/tools", ("upip.py", "upip_utarfile.py"))
