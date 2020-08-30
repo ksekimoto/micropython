ksekimoto/micropython

commit c596b64ba969019957d20411a75bb1b962b84d73

unix: Add build variants, analogous to boards on bare-metal.
Invoking "make" will still build the standard "micropython" executable, but
other variants are now build using, eg, "make VARIANT=minimal".  This
follows how bare-metal ports specify a particular board, and allows running
any make target (eg clean, test) with any variant.

Convenience targets (eg "make coverage") are provided to retain the old
behaviour, at least for now.

See issue #3043.

ports/unix/variants/fast/mpconfigvariant.h
@@ -28,8 +28,9 @@
 // synthetic benchmarking, at the expense of features supported and memory
 // usage. This config is not intended to be used in production.
 
-#include <mpconfigport.h>
 #define MICROPY_PY___FILE__ (0)
 // 91 is a magic number proposed by @dpgeorge, which make pystone run ~ at tie
 // with CPython 3.4.
 #define MICROPY_MODULE_DICT_SIZE (91)
+
+#include "variants/DEV/mpconfigvariant.h"

ports/unix/variants/fast/mpconfigvariant.mk
@@ -1,0 +1,7 @@
+# build synthetically fast interpreter for benchmarking
+
+COPT = "-O2 -DNDEBUG -fno-crossjumping"
+
+PROG = micropython_fast
+
+FROZEN_MANIFEST =

ports/unix/variants/freedos/mpconfigvariant.h
@@ -26,15 +26,15 @@
 
 // options to control how MicroPython is built
 
-#include <mpconfigport.h>
+#define MICROPY_PY_USELECT_POSIX (0)
 
-#undef MICROPY_STREAMS_NON_BLOCK
 #define MICROPY_STREAMS_NON_BLOCK (0)
 
-#undef MICROPY_PY_SYS_PLATFORM
 #define MICROPY_PY_SYS_PLATFORM "freedos"
 
 // djgpp dirent struct does not have d_ino field
 #undef _DIRENT_HAVE_D_INO
 
 #define MICROPY_USE_INTERNAL_ERRNO  (1)
+
+#include "variants/DEV/mpconfigvariant.h"

ports/unix/variants/coverage/mpconfigvariant.h
@@ -24,13 +24,11 @@
  * THE SOFTWARE.
  */
 
-// Default unix config while intended to be comprehensive, may still not enable
-// all the features, this config should enable more (testable) options.
+// This config enables almost all possible features such that it can be used
+// for coverage testing.
 
 #define MICROPY_VFS                    (1)
 #define MICROPY_PY_UOS_VFS             (1)
-
-#include <mpconfigport.h>
 
 #define MICROPY_OPT_MATH_FACTORIAL     (1)
 #define MICROPY_FLOAT_HIGH_QUALITY_HASH (1)

ports/unix/variants/coverage/mpconfigvariant.mk
@@ -1,0 +1,17 @@
+PROG ?= micropython_coverage
+
+COPT = -O0
+
+CFLAGS_EXTRA += \
+	-fprofile-arcs -ftest-coverage \
+	-Wdouble-promotion -Wformat -Wmissing-declarations -Wmissing-prototypes -Wsign-compare \
+	-Wold-style-definition -Wpointer-arith -Wshadow -Wuninitialized -Wunused-parameter \
+	-DMICROPY_UNIX_COVERAGE
+
+LDFLAGS_EXTRA += -fprofile-arcs -ftest-coverage
+
+FROZEN_MANIFEST = manifest_coverage.py
+
+MICROPY_VFS_FAT = 1
+MICROPY_VFS_LFS1 = 1
+MICROPY_VFS_LFS2 = 1

ports/unix/variants/freedos/mpconfigvariant.mk
@@ -1,0 +1,20 @@
+CC = i586-pc-msdosdjgpp-gcc
+
+STRIP = i586-pc-msdosdjgpp-strip
+
+SIZE = i586-pc-msdosdjgpp-size
+
+CFLAGS_EXTRA = \
+	-DMICROPY_NLR_SETJMP \
+	-Dtgamma=gamma \
+	-DMICROPY_EMIT_X86=0 \
+	-DMICROPY_NO_ALLOCA=1 \
+
+PROG = micropython_freedos
+
+MICROPY_PY_SOCKET = 0
+MICROPY_PY_FFI = 0
+MICROPY_PY_JNI = 0
+MICROPY_PY_BTREE = 0
+MICROPY_PY_THREAD = 0
+MICROPY_PY_USSL = 0

ports/unix/variants/minimal/mpconfigvariant.h
@@ -26,6 +26,9 @@
 
 // options to control how MicroPython is built
 
+// Prevent the rest of the default mpconfigport.h being used.
+#define MICROPY_UNIX_MINIMAL (1)
+
 #define MICROPY_ALLOC_QSTR_CHUNK_INIT (64)
 #define MICROPY_ALLOC_PARSE_RULE_INIT (8)
 #define MICROPY_ALLOC_PARSE_RULE_INC  (8)

ports/unix/variants/minimal/mpconfigvariant.mk
@@ -1,0 +1,14 @@
+# build a minimal interpreter
+COPT = -Os -DNDEBUG
+
+PROG = micropython_minimal
+
+FROZEN_MANIFEST =
+
+MICROPY_PY_BTREE = 0
+MICROPY_PY_FFI = 0
+MICROPY_PY_SOCKET = 0
+MICROPY_PY_THREAD = 0
+MICROPY_PY_TERMIOS = 0
+MICROPY_PY_USSL = 0
+MICROPY_USE_READLINE = 0

commit 772632e93754b4e4eca12ef09c4586cce3d7caec

unix/variants: Fix fast and freedos variants so they build again.
This regressed in c596b64ba969019957d20411a75bb1b962b84d73

Signed-off-by: Damien George <damien@micropython.org>

ports/unix/variants/fast/mpconfigvariant.h
@@ -32,5 +32,3 @@
 // 91 is a magic number proposed by @dpgeorge, which make pystone run ~ at tie
 // with CPython 3.4.
 #define MICROPY_MODULE_DICT_SIZE (91)
-
-#include "variants/DEV/mpconfigvariant.h"

ports/unix/variants/fast/mpconfigvariant.mk
@@ -1,6 +1,6 @@
 # build synthetically fast interpreter for benchmarking
 
-COPT += "-fno-crossjumping -O2"
+COPT += -fno-crossjumping -O2
 
 PROG = micropython-fast
 

ports/unix/variants/freedos/mpconfigvariant.h
@@ -36,5 +36,3 @@
 #undef _DIRENT_HAVE_D_INO
 
 #define MICROPY_USE_INTERNAL_ERRNO  (1)
-
-#include "variants/DEV/mpconfigvariant.h"