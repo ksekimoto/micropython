0e6a8082fe736eaedfbce723a2f40417dc812c8f
ksekimoto <ksekimoto@users.noreply.github.com>
2020-09-20 17:40:23 +0900

    Fixed drag&drop binary build issue

--- ports/rz/build_gr_mango.sh
+++ ports/rz/build_gr_mango.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 set -eu -o pipefail
-make V=1 DEBUG=1 BOARD=GR_MANGO clean 2>&1 | tee GR_MANGO_build.log
-make V=1 DEBUG=1 BOARD=GR_MANGO MICROPY_PY_ESP8266=1 MICROPY_PY_LWIP=1 MICROPY_SSL_MBEDTLS=1 MICROPY_PY_USSL=1 2>&1 | tee GR_MANGO_build.log
-make V=1 DEBUG=1 BOARD=GR_MANGO_DD clean 2>&1 | tee GR_MANGO_DD_build.log
-make V=1 DEBUG=1 BOARD=GR_MANGO_DD  MICROPY_PY_ESP8266=1 MICROPY_PY_LWIP=1 MICROPY_SSL_MBEDTLS=1 MICROPY_PY_USSL=1 2>&1 | tee GR_MANGO_DD_build.log
+export BOARD="GR_MANGO"
+DT=`date +%Y%m%d%H%M`
+make V=1 DEBUG=1 BOARD=${BOARD} clean 2>&1 | tee ${BOARD}_build_${DT}.log
+make V=1 DEBUG=1 BOARD=${BOARD} clean-mbed 2>&1 | tee -a ${BOARD}_build_${DT}.log
+make V=1 DEBUG=1 BOARD=${BOARD} MICROPY_PY_ESP8266=1 MICROPY_PY_LWIP=1 MICROPY_SSL_MBEDTLS=1 MICROPY_PY_USSL=1 2>&1 | tee -a ${BOARD}_build_${DT}.log
+export BOARD="GR_MANGO_DD"
+DT=`date +%Y%m%d%H%M`
+make V=1 DEBUG=1 BOARD=${BOARD} clean 2>&1 | tee ${BOARD}_build_${DT}.log
+make V=1 DEBUG=1 BOARD=${BOARD} clean-mbed 2>&1 | tee -a ${BOARD}_build_${DT}.log
+make V=1 DEBUG=1 BOARD=${BOARD} MICROPY_PY_ESP8266=1 MICROPY_PY_LWIP=1 MICROPY_SSL_MBEDTLS=1 MICROPY_PY_USSL=1 2>&1 | tee -a ${BOARD}_build_${DT}.log
--- ports/rz/build_gr_mango_beta.sh
+++ ports/rz/build_gr_mango_beta.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 set -eu -o pipefail
-make V=1 DEBUG=1 BOARD=GR_MANGO_BETA clean 2>&1 | tee GR_MANGO_BETA_build.log
-make V=1 DEBUG=1 BOARD=GR_MANGO_BETA MICROPY_PY_ESP8266=1 MICROPY_PY_LWIP=1 MICROPY_SSL_MBEDTLS=1 MICROPY_PY_USSL=1 2>&1 | tee GR_MANGO_BETA_build.log
-make V=1 DEBUG=1 BOARD=GR_MANGO_BETA_DD clean 2>&1 | tee GR_MANGO_BETA_DD_build.log
-make V=1 DEBUG=1 BOARD=GR_MANGO_BETA_DD  MICROPY_PY_ESP8266=1 MICROPY_PY_LWIP=1 MICROPY_SSL_MBEDTLS=1 MICROPY_PY_USSL=1 2>&1 | tee GR_MANGO_BETA_DD_build.log
+export BOARD="GR_MANGO_BETA"
+DT=`date +%Y%m%d%H%M`
+make V=1 DEBUG=1 BOARD=${BOARD} clean 2>&1 | tee ${BOARD}_build_${DT}.log
+make V=1 DEBUG=1 BOARD=${BOARD} clean-mbed 2>&1 | tee -a ${BOARD}_build_${DT}.log
+make V=1 DEBUG=1 BOARD=${BOARD} MICROPY_PY_ESP8266=1 MICROPY_PY_LWIP=1 MICROPY_SSL_MBEDTLS=1 MICROPY_PY_USSL=1 2>&1 | tee -a ${BOARD}_build_${DT}.log
+export BOARD="GR_MANGO_BETA_DD"
+DT=`date +%Y%m%d%H%M`
+make V=1 DEBUG=1 BOARD=${BOARD} clean 2>&1 | tee ${BOARD}_build_${DT}.log
+make V=1 DEBUG=1 BOARD=${BOARD} clean-mbed 2>&1 | tee -a ${BOARD}_build_${DT}.log
+make V=1 DEBUG=1 BOARD=${BOARD} MICROPY_PY_ESP8266=1 MICROPY_PY_LWIP=1 MICROPY_SSL_MBEDTLS=1 MICROPY_PY_USSL=1 2>&1 | tee -a ${BOARD}_build_${DT}.log
