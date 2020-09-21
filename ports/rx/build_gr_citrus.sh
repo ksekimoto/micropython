45d35a091af0a98209f63699337c494a95432e75
ksekimoto <ksekimoto@users.noreply.github.com>
2020-09-21 12:48:58 +0900

    Changed build log file name

--- ports/rx/build_gr_citrus.sh
+++ ports/rx/build_gr_citrus.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 set -eu -o pipefail
-make V=1 DEBUG=1 BOARD=GR_CITRUS clean 2>&1 | tee GR_CITRUS_build.log
-make V=1 DEBUG=1 BOARD=GR_CITRUS 2>&1 | tee GR_CITRUS_build.log
-make V=1 DEBUG=1 BOARD=GR_CITRUS_DD clean 2>&1 | tee GR_CITRUS_DD_build.log
-make V=1 DEBUG=1 BOARD=GR_CITRUS_DD 2>&1 | tee GR_CITRUS_DD_build.log
+export BOARD="GR_CITRUS"
+DT=`date +%Y%m%d%H%M`
+make V=1 DEBUG=1 BOARD=${BOARD} clean 2>&1 | tee ${BOARD}_build_${DT}.log
+make V=1 DEBUG=1 BOARD=${BOARD} MICROPY_PY_ESP8266=1 MICROPY_PY_LWIP=1 MICROPY_SSL_MBEDTLS=1 MICROPY_PY_USSL=1 2>&1 | tee -a ${BOARD}_build_${DT}.log
+export BOARD="GR_CITRUS_DD"
+DT=`date +%Y%m%d%H%M`
+make V=1 DEBUG=1 BOARD=${BOARD} clean 2>&1 | tee ${BOARD}_build_${DT}.log
+make V=1 DEBUG=1 BOARD=${BOARD} MICROPY_PY_ESP8266=1 MICROPY_PY_LWIP=1 MICROPY_SSL_MBEDTLS=1 MICROPY_PY_USSL=1 2>&1 | tee -a ${BOARD}_build_${DT}.log
--- ports/rx/build_gr_sakura.sh
+++ ports/rx/build_gr_sakura.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 set -eu -o pipefail
-make V=1 DEBUG=1 BOARD=GR_SAKURA clean 2>&1 | tee GR_SAKURA_build.log
-make V=1 DEBUG=1 BOARD=GR_SAKURA 2>&1 | tee GR_SAKURA_build.log
-make V=1 DEBUG=1 BOARD=GR_SAKURA_DD clean 2>&1 | tee GR_SAKURA_DD_build.log
-make V=1 DEBUG=1 BOARD=GR_SAKURA_DD 2>&1 | tee GR_SAKURA_DD_build.log
+export BOARD="GR_SAKURA"
+DT=`date +%Y%m%d%H%M`
+make V=1 DEBUG=1 BOARD=${BOARD} clean 2>&1 | tee ${BOARD}_build_${DT}.log
+make V=1 DEBUG=1 BOARD=${BOARD} 2>&1 | tee -a ${BOARD}_build_${DT}.log
+export BOARD="GR_SAKURA_DD"
+DT=`date +%Y%m%d%H%M`
+make V=1 DEBUG=1 BOARD=${BOARD} clean 2>&1 | tee ${BOARD}_build_${DT}.log
+make V=1 DEBUG=1 BOARD=${BOARD} 2>&1 | tee -a ${BOARD}_build_${DT}.log
